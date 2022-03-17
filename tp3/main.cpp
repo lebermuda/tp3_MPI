f74ab20aad2f5fd8460515d4736c12d14fcbf2eb
lebermuda <jean35lb@gmail.com>
2022-03-17 16:57:08 -0400

    test hello world avec MPI

--- tp3/main.cpp
+++ tp3/main.cpp
@@ -8,6 +8,8 @@
 #include <ctime>
 #include <iostream>
 #include <stdexcept>
+#include <mpi.h>
+
 
 using namespace std;
 
@@ -66,6 +68,27 @@ void invertSequential(Matrix& iA) {
 // Inverser la matrice par la méthode de Gauss-Jordan; implantation MPI parallèle.
 void invertParallel(Matrix& iA) {
     // vous devez coder cette fonction
+    MPI_Init(NULL,NULL);
+    
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    MPI_Finalize();
+
 }
 
 // Multiplier deux matrices.
@@ -105,6 +128,8 @@ int main(int argc, char** argv) {
     cout << "Produit des deux matrices:\n" << lRes.str() << endl;
 
     cout << "Erreur: " << lRes.getDataArray().sum() - lS << endl;
+
+    invertParallel(lB);
 
     return 0;
 }
